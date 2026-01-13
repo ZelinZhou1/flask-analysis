85dee5a88f621e37a31faaa3a0b30fd958891168
ZelinZhou1 <2594871384@qq.com>
2026-01-13 13:25:33 +0800

    feat: init collectors package

---
+++ src/collectors/__init__.py
@@ -0,0 +1,11 @@
+"""
+采集器模块初始化文件
+导出各种采集器类以便外部使用
+"""
+
+from .pydriller_collector import PyDrillerCollector
+from .branch_collector import BranchCollector
+from .tag_collector import TagCollector
+from .github_api import GitHubCollector
+
+__all__ = ["PyDrillerCollector", "BranchCollector", "TagCollector", "GitHubCollector"]
