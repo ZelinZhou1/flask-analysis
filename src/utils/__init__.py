f259de49815f8b941e7531c485c846af801d4b3f
lff20041215 <2134496364@qq.com>
2026-01-13 11:05:33 +0800

    feat: init utils package

---
+++ src/utils/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+工具模块包
+提供字体配置、辅助函数、日期处理、缓存管理和数据持久化功能
+"""
+
+from .font_config import configure_chinese_font, get_chinese_font
+from .helpers import truncate_label, safe_divide, format_number, ensure_dir
+from .date_utils import parse_date, get_year, get_month, get_weekday, get_hour
+from .cache import CacheManager
+from .persistence import save_json, load_json, save_csv, load_csv
+
+__all__ = [
+    "configure_chinese_font",
+    "get_chinese_font",
+    "truncate_label",
+    "safe_divide",
+    "format_number",
+    "ensure_dir",
+    "parse_date",
+    "get_year",
+    "get_month",
+    "get_weekday",
+    "get_hour",
+    "CacheManager",
+    "save_json",
+    "load_json",
+    "save_csv",
+    "load_csv",
+]
